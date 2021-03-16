d6d4ad01dc173788d04ad1fd557d553cd5f62425
Akshay-cita <edakkatakshay@gmail.com>
2021-03-16 18:07:51 +0530

    code updated with async and .then function

--- server.js
+++ server.js
@@ -29,14 +29,14 @@ app.post('/signup',jsonparser,async(req,res)=>{
         }
         else {
             await newuser.save().then((err,doc)=>{
-                return res.status(200).json({success:true,message:"User created",user : doc});
+                return res.status(200).json({success:true,message:"User created",user : doc});  
             }).catch(err =>{
                 return res.status(500).json({error:true,message:"User not created please fill required fields", error_details:err.message});
-            })     
+            });
         }
-        
-        });
     });
+});
+
 
 
 
@@ -77,71 +77,53 @@ app.post('/signin',jsonparser,function(req,res){
 
 
 //create products
-app.post('/product/create',jsonparser,function(req,res){
+app.post('/product/create',jsonparser,async (req,res)=>{
+    try{
     var newprod= new Product(req.body);
-
-    Product.findOne({productcode:newprod.productcode},function(err,prod){
-        if(prod){
-            return res.status(500).json({error:true,message:"Product already exist"});
-        }
-        newprod.save((err,doc)=>{
-            if(err){
-                return res.status(500).json({error:true,message:"Product not created "});
-            }
-            else{
-                return res.status(200).json({success:true,message:"Product created "});
-            }
-        });
-    });
+    const prod= await Product.findOne({productcode:newprod.productcode});
+    console.log(prod);
+    if(prod) throw new Error("product already exists");
+    await newprod.save();
+    return res.status(200).json({success:true,message:"Product created "});
+    }catch(err){
+        return res.status(500).send({error:true,message:""+ err});
+    }
 });
+
 
 //list of products
-app.get('/product/list',function(req,res){
-
-    Product.find({},function(err,cursor){
-        if(err){
-            return res.status(500).json({error:true,message:"Product data not retireved"});
-        }
-        else{
-            return res.status(200).json({success:true,message:"Data retrieved",data:cursor});
-        }
+app.get('/product/list',async (req,res)=>{
+    await Product.find({}).then(cursor=>{
+        return res.status(200).json({success:true,message:"Data retrieved",data:cursor}); 
+    }).catch(err=>{
+        return res.status(500).json({error:true,message:"Product data not retireved"});
     });
-
 });
+
 //product delete
-app.delete('/product/list/:id',function(req,res){
-    Product.findByIdAndRemove(req.params.id,function(err){
-        if(err){
-            return res.status(500).json({error:true,message:"Product not removed"});
-        }
-        else{
-            return res.status(200).json({success:true,message:"Product deleted"});
-        }
+app.delete('/product/list/:id',(req,res)=>{
+    Product.findByIdAndRemove(req.params.id).then(()=>{
+        return res.status(200).json({success:true,message:"Product deleted"})
+    }).catch(err=>{
+        return res.status(500).json({error:true,message:"Product not removed"});
     });
 });
 //product list by id
-app.get('/product/:id',function(req,res){
-    Product.findById(req.params.id,function(err,data){
-        if(err){
-            return res.status(500).json({error:true,message:"Product not exist"});
-        }
-        else{
-            return res.status(200).json({success:true,message:"Found product",curr_data:data});
-        }
+app.get('/product/:id',(req,res)=>{
+    Product.findById(req.params.id).then(data=>{
+        return res.status(200).json({success:true,message:"Found product",curr_data:data})
+        }).catch(err=>{
+            return res.status(500).json({error:true,message:"Product not exist in list"});
     });
 });
 
 //product update 
-app.put('/product/list/:id',jsonparser,function(req,res){
+app.put('/product/list/:id',jsonparser,(req,res)=>{
     var newvalue={$set: {productname: req.body.productname,productcode:req.body.productcode,price:req.body.price}};
-    Product.updateOne({_id:req.params.id},newvalue,function(err,data){
-        if(err){
-            return res.status(500).json({error:true,message:"Product Updation failed"});
-        }
-        else{
-            return res.status(200).json({success:true,message:"Product Updated successfully", Updated_data:data});
-            
-        }
+     Product.updateOne({_id:req.params.id},newvalue).then(data =>{
+        return res.status(200).json({success:true,message:"Product Updated successfully", Updated_data:data})
+        }).catch(err=>{
+            return res.status(500).json({error:true,message:"Product Updation failed",err_msg:err.message});
     });
 });
 
